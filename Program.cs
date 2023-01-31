8a0e3915a7348d3b4b39b50716f2f6382ff5a16c
Дарья <red_peril@bk.ru>
2023-01-31 16:14:02 +0300

    задача 13

--- Program.cs
+++ Program.cs
@@ -1,8 +1,27 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
+/*
 Console.Clear();
 Console.WriteLine("Задача 10 ");
 Console.Write("Введи трёхзначное число: ");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
 Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+*/
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/*
+Console.WriteLine("Задача 13 ");
+
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+Console.WriteLine("-> третьей цифры нет");
+}
+*/
+
